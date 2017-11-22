75ff1beb861c1c24cfd4e47ecef1eb50a4525ee1
Rayms <rayms.dev@gmail.com>
2017-11-22 16:17:55 +0100

    add angular material and sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {AngularFireModule} from 'angularfire2';
 import {AngularFireDatabaseModule} from 'angularfire2/database';
 import {AngularFireAuthModule} from 'angularfire2/auth';
-
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { MapComponent } from './map/map.component';
 
+const APP_COMPONENTS = [
+  AppComponent,
+  SignInComponent,
+  MapComponent,
+];
+
+const MATERIAL_COMPONENTS = [
+  MatSidenavModule,
+  MatButtonModule
+];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SignInComponent,
-    MapComponent
+    ...APP_COMPONENTS,
+    
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    ...MATERIAL_COMPONENTS,
     AngularFireModule.initializeApp(environment.firebase, "bring-me-out"),
     AngularFireDatabaseModule,
     AngularFireAuthModule
